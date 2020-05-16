a423e25b0d23291bdf609a01bede4ce4db25f359
mirror6Y <1766650501@qq.com>
2020-05-16 22:22:17 +0800

    前后端分离

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import User from './service/user/user'
 import 'antd/dist/antd.css';
 import * as serviceWorker from './serviceWorker';
--- react/my-app/src/service/user/user.jsx
+++ react/my-app/src/service/user/user.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Table,Button,Input,Popconfirm } from 'antd';
+import { Table, Button, Input, Popconfirm } from 'antd';
 import { SmileOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 class User extends Component {
 
@@ -9,18 +10,20 @@ class User extends Component {
 
         this.state = {
 
-            dataSource: [
-                { key: 1, nid: 1, name: 'tab', gender: '男', age: 22, schoolname: '第1中学', description: '热爱班级活动，尊敬老师' },
-                { key: 2, nid: 2, name: 'shift', gender: '男', age: 22, schoolname: '第2中学', description: '热爱班级活动，尊敬老师' },
-                { key: 6, nid: 6, name: 'ctrl', gender: '男', age: 22, schoolname: '第3中学', description: '热爱班级活动，尊敬老师' },
-                { key: 4, nid: 4, name: 'caps lock', gender: '男', age: 22, schoolname: '第4中学', description: '热爱班级活动，尊敬老师' },
-                { key: 5, nid: 5, name: 'enter', gender: '女', age: 22, schoolname: '第5中学', description: '热爱班级活动，尊敬老师' }
-            ],
-            index: '',
-            PersonCount: 0,
-            selectedRowKeys: [],
-            selectedRows: [],
-            record: 'abc'
+            // dataSource: [
+            //     { key: 1, nid: 1, name: 'tab', gender: '男', age: 22, schoolname: '第1中学', description: '热爱班级活动，尊敬老师' },
+            //     { key: 2, nid: 2, name: 'shift', gender: '男', age: 22, schoolname: '第2中学', description: '热爱班级活动，尊敬老师' },
+            //     { key: 6, nid: 6, name: 'ctrl', gender: '男', age: 22, schoolname: '第3中学', description: '热爱班级活动，尊敬老师' },
+            //     { key: 4, nid: 4, name: 'caps lock', gender: '男', age: 22, schoolname: '第4中学', description: '热爱班级活动，尊敬老师' },
+            //     { key: 5, nid: 5, name: 'enter', gender: '女', age: 22, schoolname: '第5中学', description: '热爱班级活动，尊敬老师' }
+            // ],
+            // index: '',
+            // PersonCount: 0,
+            // selectedRowKeys: [],
+            // selectedRows: [],
+            // record: 'abc',
+            users:[],
+            isLoaded:false
 
         };
 
@@ -29,19 +32,19 @@ class User extends Component {
         this.handleSelectedDelete = this.handleSelectedDelete.bind(this);
 
         this.columns = [
-            { title: '编号', dataIndex: 'nid', key: 'nid', width: '8%' },
+            { title: '账号', dataIndex: 'account', key: 'account', width: '8%' },
             { title: '姓名', dataIndex: 'name', key: 'name', width: '15%' },
             { title: '性别', dataIndex: 'gender', key: 'gender', width: '10%' },
-            { title: '年龄', dataIndex: 'age', key: 'age', width: '15%', },
-            { title: '学校', dataIndex: 'schoolname', key: 'schoolname', width: '15%' },
-            { title: '在校表现', dataIndex: 'description', key: 'description', width: '20%' },
+            { title: '手机号码', dataIndex: 'tel', key: 'tel', width: '15%', },
+            { title: '电子邮箱', dataIndex: 'email', key: 'email', width: '15%' },
+            { title: '是否启用', dataIndex: 'enabled', key: 'enabled', width: '20%' },
             {
                 title: '操作', dataIndex: '', key: 'operation', width: '32%', render: (text, record, index) => (
                     <span>
                         <Popconfirm title="删除不可恢复，你确定要删除吗?" >
                             <a title="用户删除" className="mgl10" onClick={this.onDelete.bind(this, index)}>
-                            <SmileOutlined /></a>
-                                
+                                <SmileOutlined /></a>
+
 
                         </Popconfirm>
                         <span className="ant-divider" />
@@ -52,11 +55,29 @@ class User extends Component {
 
     }
 
+    componentDidMount() {
+        console.log('getUserList');
+        const url = "/system/user/getUserList";
+        const _this=this;
+        axios.get(url)
+            .then(function (response) {
+                console.log(response.data.data.records)
+
+                _this.setState({
+                    users: response.data.data.records,
+                    isLoaded: true
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     //得到子元素传过来的值
     appendPerson(event) {
         let array = [];
         let count = 0;
-        this.state.dataSource.forEach(function (element) {
+        this.state.users.forEach(function (element) {
             Object.keys(element).some(function (key) {
                 if (key === 'nid') {
                     count++;
@@ -65,28 +86,28 @@ class User extends Component {
             })
         })
         let sortData = array.sort();//对遍历得到的数组进行排序
-        let MaxData = sortData[(this.state.dataSource.length) - 1]//取最后一位下标的值
+        let MaxData = sortData[(this.state.users.length) - 1]//取最后一位下标的值
         event.key = MaxData + 1;
         event.nid = MaxData + 1;
         this.setState({
-            dataSource: [...this.state.dataSource, event]
+            users: [...this.state.users, event]
         })
 
     }
 
     onDelete(index) {
         console.log(index)
-        const dataSource = [...this.state.dataSource];
-        dataSource.splice(index, 1);//index为获取的索引，后面的 1 是删除几行
-        this.setState({ dataSource });
+        const users = [...this.state.users];
+        users.splice(index, 1);//index为获取的索引，后面的 1 是删除几行
+        this.setState({ users });
     }
 
     handleSelectedDelete() {
         if (this.state.selectedRowKeys.length > 0) {
             console.log(...this.state.selectedRowKeys)
-            const dataSource = [...this.state.dataSource]
-            dataSource.splice(this.state.selectedRows, this.state.selectedRows.length)
-            this.setState({ dataSource });
+            const users = [...this.state.users]
+            users.splice(this.state.selectedRows, this.state.selectedRows.length)
+            this.setState({ users });
         }
         else {
 
@@ -107,7 +128,7 @@ class User extends Component {
                         </div>
                     </div>
                     <Table columns={this.columns}
-                        dataSource={this.state.dataSource}
+                    dataSource={this.state.users}
                         className="table"
                         // rowSelection={rowSelection}
                         scroll={{ y: 400 }} />
