8f7b7bd996c3fff8cb12e1377d59da8b9f4620ad
mirror6Y <1766650501@qq.com>
2020-05-31 22:23:01 +0800

    axios post到后台  数据为空

--- react/my-app/src/service/user/userAdd.jsx
+++ react/my-app/src/service/user/userAdd.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import { Form, Input, Button, Select, Modal, Radio } from 'antd'
+import axios from 'axios';
+import qs from 'qs'
 const FormItem = Form.Item;
 const Option = Select.Option;
 
+
 class UserAdd extends React.Component {
+
+    formRef = React.createRef();
+
 
     constructor(props) {
         super(props);
 
         this.state = {
-            visible: false,
-            loading: false
+
         };
 
-        this.handleAdd = this.handleAdd.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
-        // this.handleOk = this.handleOk.bind(this)
-        // this.handleClear = this.handleClear.bind(this)
     }
-
-    handleAdd() {
-        this.setState({
-            visible: true
-        });
-    }
-
 
 
 
     handleOk = () => {
-       
-        console.log("提交")
+
+        console.log(this.formRef.current.getFieldsValue())
+
+        let user = this.formRef.current.getFieldsValue();
+
+        const url = "/system/user/addUser";
+        const _this = this;
+        axios.post(url,
+            {
+                data: qs.stringify(user)
+            })
+            .then(function (response) {
+                _this.props.toggleVisible(false)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
     };
 
     handleCancel = () => {
@@ -41,7 +51,6 @@ class UserAdd extends React.Component {
     render() {
 
 
-        // const { visible, loading } = this.state;
         const { visible } = this.props
 
         const formItemLayout = {
@@ -50,57 +59,57 @@ class UserAdd extends React.Component {
         };
 
         return (
-            <div>
 
-                <Modal visible={visible} title="添加用户" onCancel={this.handleCancel} onOk={this.handleOk}>
 
-                    <Form   {...formItemLayout} >
+            <Modal visible={visible} title="添加用户" onCancel={this.handleCancel} onOk={this.handleOk}>
 
-                        <FormItem name="account" label="账号" rules={[
-                            {
-                                required: true,
-                                message: '请输入账号',
-                            },
-                        ]} >
-                            <Input placeholder="请输入账号" />
-                        </FormItem>
+                <Form ref={this.formRef} {...formItemLayout} >
 
-                        <FormItem name="name" label="姓名" rules={[
-                            {
-                                required: true,
-                                message: '请输入姓名',
-                            },
-                        ]} >
-                            <Input placeholder="请输入姓名" />
-                        </FormItem>
+                    <FormItem name="account" label="账号" rules={[
+                        {
+                            required: true,
+                            message: '请输入账号',
+                        },
+                    ]} >
+                        <Input placeholder="请输入账号" />
+                    </FormItem>
 
-                        <FormItem name="gender" label="性别">
-                            <Radio.Group>
-                                <Radio value="1">男</Radio>
-                                <Radio value="0">女</Radio>
-                            </Radio.Group>
-                        </FormItem>
+                    <FormItem name="name" label="姓名" rules={[
+                        {
+                            required: true,
+                            message: '请输入姓名',
+                        },
+                    ]} >
+                        <Input placeholder="请输入姓名" />
+                    </FormItem>
 
-                        <FormItem name="tel" label="手机号码" >
-                            <Input placeholder="请输入手机号码" />
-                        </FormItem>
+                    <FormItem name="gender" label="性别">
+                        <Radio.Group>
+                            <Radio value="1">男</Radio>
+                            <Radio value="0">女</Radio>
+                        </Radio.Group>
+                    </FormItem>
 
-                        <FormItem name="email" label="电子邮箱" >
-                            <Input placeholder="请输入电子邮箱" />
-                        </FormItem>
+                    <FormItem name="tel" label="手机号码" >
+                        <Input placeholder="请输入手机号码" />
+                    </FormItem>
 
-                        <FormItem name="select" label="角色" hasFeedback
-                            rules={[{ required: true, message: '请选择角色!' }]}
-                        >
-                            <Select placeholder="请选择角色">
-                                <Option value="1">普通用户</Option>
-                                <Option value="2">管理员</Option>
-                            </Select>
-                        </FormItem>
+                    <FormItem name="email" label="电子邮箱" >
+                        <Input placeholder="请输入电子邮箱" />
+                    </FormItem>
 
-                    </Form>
-                </Modal>
-            </div>
+                    <FormItem name="select" label="角色" hasFeedback
+                        rules={[{ required: true, message: '请选择角色!' }]}
+                    >
+                        <Select placeholder="请选择角色">
+                            <Option value="1">普通用户</Option>
+                            <Option value="2">管理员</Option>
+                        </Select>
+                    </FormItem>
+
+                </Form>
+            </Modal>
+
         )
     }
 }
